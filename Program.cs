7b346db9ffd9390985e896a60e0cfc27de8591b6
Arsenicu <kubarodak2003@outlook.com>
2024-08-27 21:51:05 +0100

    initial code structure

---
+++ Program.cs
@@ -0,0 +1,76 @@
+﻿using System.Diagnostics;
+using System.Reflection;
+using System.Globalization;
+
+namespace VRMemories
+{
+    class Program
+    {
+        static int cooldown = 300000; //5 minutes
+
+        static void Main(string[] args)
+        {
+            UnpackAssembly();
+
+            //TestOpenVRApi();
+
+            ScreenshotLoop();
+        }
+
+        private static void ScreenshotLoop()
+        {
+            VRApp vrapp = new VRApp();
+            long target = DateTimeOffset.Now.ToUnixTimeMilliseconds();
+
+            while (true) //mental anguish.
+            {
+                if (DateTimeOffset.Now.ToUnixTimeMilliseconds() >= target)
+                {
+                    vrapp.Capture();
+                    target = DateTimeOffset.Now.ToUnixTimeMilliseconds() + cooldown;
+                }
+            }
+        }
+
+        private static void TestOpenVRApi()
+        {
+            Valve.VR.OpenVR.Chaperone.AreBoundsVisible();
+        }
+
+        private static void PrintAssemblyNames()
+        {
+            var resourceNames = Assembly.GetExecutingAssembly().GetManifestResourceNames();
+            foreach (var resourceName in resourceNames)
+            {
+                Console.WriteLine(resourceName);
+            }
+        }
+
+        private static void UnpackAssembly()
+        {
+            string fileName = "openvr_api.dll";
+            if (File.Exists(fileName))
+                return;
+
+            var executingAssembly = Assembly.GetExecutingAssembly();
+
+            var path = "VRmemories." + fileName;
+
+            using (Stream stream = executingAssembly.GetManifestResourceStream("VRMemories.openvr_api.dll"))
+            {
+                if (stream == null)
+                    return;
+
+                var assemblyRawBytes = new byte[stream.Length];
+                stream.Read(assemblyRawBytes, 0, assemblyRawBytes.Length);
+
+                using (FileStream fs = new FileStream("openvr_api.dll", FileMode.CreateNew))
+                {
+                    fs.Write(assemblyRawBytes);
+                }
+            }
+
+
+        }
+    }
+}
---
+++ VRApp.cs
@@ -0,0 +1,53 @@
+﻿using OVRSharp.Exceptions;
+using OVRSharp.Graphics.DirectX;
+using Valve.VR;
+using System;
+using System.Drawing;
+using System.Diagnostics;
+
+namespace VRMemories
+{
+    public class VRApp : OVRSharp.Application
+    {
+        DirectXCompositor compositor;
+        string path;
+
+        public VRApp() : base(ApplicationType.Background)
+        {
+            this.compositor = new DirectXCompositor();
+
+            //default
+            path = Path.Join(Environment.GetFolderPath(Environment.SpecialFolder.MyPictures), "VRMemories");
+            if(!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+        }
+
+        public void Capture()
+        {
+            Bitmap bt = compositor.GetMirrorImage();
+            string fp = Path.Join(path, GetUniqueFilename());
+
+            Console.Write($"Capturing {fp} ... ");
+            Debug.Write($"Capturing {fp} ... ");
+
+            using (FileStream fs = new FileStream(fp, FileMode.OpenOrCreate))
+            {
+                bt.Save(fs, System.Drawing.Imaging.ImageFormat.Png);
+                bt.Dispose();
+            }
+
+            Console.Write("complete.\n");
+            Debug.Write("complete.\n");
+        }
+
+        public string GetUniqueFilename()
+        {
+            DateTime d = DateTime.Now;
+
+            //YYYY-MM-DD_HH-mm-ss-SSS
+            return $"{d.Year}-{(d.Month + "").PadLeft(2, '0')}-{(d.Day + "").PadLeft(2, '0')}_{(d.Hour + "").PadLeft(2, '0')}-{(d.Minute + "").PadLeft(2, '0')}-{(d.Second + "").PadLeft(2, '0')}-{(d.Millisecond + "").PadLeft(3, '0')}.png";
+        }
+    }
+}
