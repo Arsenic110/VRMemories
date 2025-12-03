667e43bbd94e46323070771874c42d34f20518f4
Arsenicu <kubarodak2003@outlook.com>
2025-12-03 20:29:07 +0000

    configure folders

--- src/capture.rs
+++ src/capture.rs
@@ -1,33 +1,44 @@
 use std::time::Instant;
 use windows_capture::{
     capture::{Context, GraphicsCaptureApiHandler},
-    encoder::{ImageEncoder, VideoEncoder}, frame::ImageFormat, settings::ColorFormat
+    encoder::{ImageEncoder, VideoEncoder}, frame::ImageFormat, settings::ColorFormat,
+    graphics_capture_api::InternalCaptureControl,
+    frame::Frame
 };
 
 pub struct Capture {
+    savepath: String
 }
 
 impl GraphicsCaptureApiHandler for Capture {
     type Flags = String;
     type Error = Box<dyn std::error::Error + Send + Sync>;
 
-    fn new(_: Context<Self::Flags>) -> Result<Self, Self::Error> {
-        Ok(Self { })
+    fn new(ctx: Context<Self::Flags>) -> Result<Self, Self::Error> {
+        Ok(Self { savepath: ctx.flags })
     }
 
-    fn on_frame_arrived(
-            &mut self,
-            frame: &mut windows_capture::frame::Frame,
-            _capture_control: windows_capture::graphics_capture_api::InternalCaptureControl,
-        ) -> Result<(), Self::Error> {
-            
-        let dirs = directories::UserDirs::new().unwrap();
+    fn on_frame_arrived(&mut self, frame: &mut Frame, _capture_control: InternalCaptureControl) -> Result<(), Self::Error> {
+        let now = chrono::Local::now();
 
-        let filename = format!("{}/VRMemories/{}.png",
-            dirs.picture_dir().unwrap().display(),
-            chrono::Local::now().format("%Y-%m-%d_%H-%M-%S-%f")
+        let savefolder = format!("{}/{}", 
+            self.savepath,
+            now.format("%Y-%m")
         );
-        
+
+        let savefolder = savefolder.as_str();
+
+        match std::fs::create_dir_all(savefolder) {
+            Ok(_) => println!("Save directory didn't exist. It now does!"),
+            Err(_) => {}
+        }
+
+        let filename = format!("{}/{}.png", 
+            savefolder,
+            now.format("%Y-%m-%d_%H-%M-%S-%f")
+        );
+
+        println!("Saved {}", filename);
         frame.buffer_without_title_bar()?.save_as_image(filename, ImageFormat::Png)?;
 
         Ok(())
--- src/main.rs
+++ src/main.rs
@@ -2,22 +2,27 @@ use std::time::Duration;
 use std::thread;
 
 use windows_capture:: {
-    capture::GraphicsCaptureApiHandler, settings::*, window::{self, Window}
+    capture::GraphicsCaptureApiHandler, settings::*, window::Window
 };
 
 mod example;
 mod capture;
 
+use capture::Capture;
+
 fn main() {
-    let target_title = "VRChat";
+    let target_title = "GitHub Desktop";
     let capture_interval = Duration::from_secs(60);
+
+    let dirs = directories::UserDirs::new().unwrap();
+    let savepath = format!("{}/VRMemories", dirs.picture_dir().unwrap().display());
 
     loop {
         //attempt to find window
         let window = match Window::from_name(target_title) {
             Ok(window) => window,
             Err(_) => {
-                println!("Window not found. Next attempt in {}", capture_interval.as_secs());
+                println!("Window not found. Next attempt in {}s", capture_interval.as_secs());
                 thread::sleep(capture_interval);
                 continue;
             }
@@ -31,9 +36,9 @@ fn main() {
             MinimumUpdateIntervalSettings::Custom(capture_interval),
             DirtyRegionSettings::Default,
             ColorFormat::Rgba8,
-            "".into()
+            savepath.clone()
         );
 
-        capture::Capture::start(settings).expect("Capture Failed");
+        Capture::start(settings).expect("Capture Failed");
     }
 }
